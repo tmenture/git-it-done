02a183127d1e6ab07e7cf9d0cd3cf3e43b2fe612
tmenture <thomasoxemail@gmail.com>
2022-04-05 13:05:53 -0400

    users can now search for featred repos based on the language used

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user) {
     // format github api url
@@ -20,6 +21,20 @@ var getUserRepos = function(user) {
     .catch(function(error){
         // notice this `.catch()` getting chained nto the end ot the `.then()` method
         alert("Unable to connect to GitHub");
+    });
+};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
     });
 };
 
@@ -77,4 +92,15 @@ var displayRepos = function(repos, searchTerm) {
     }
 }
 
+var buttonClickHandeler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    
+    if (language) {
+        getFeaturedRepos(language);
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandeler);
